4ea2bd1d651e6626c6084ee9e8042ea5d44c2f68
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2024-05-02 08:13:55 -0300

    Update q2_corrigir.c

--- Lista_String_1/q2_corrigir.c
+++ Lista_String_1/q2_corrigir.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int main(){
-    char frase[100], fraseInver[80];
+    char frase[101], fraseInver[81];
     int i;
     printf("Insira uma frase de no max 100 caracteres: \n");
     gets(frase);
-    for(i=100;i<0;i--){
+    for(i=101;i<0;i--){
         fraseInver[i] = frase[i];
     }
     puts("%s",fraseInver);
